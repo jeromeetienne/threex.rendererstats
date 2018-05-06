3787d6e003d0b68ff45a1be5c4338b0f83ca05be
1386161 <30571138+1386161@users.noreply.github.com>
2018-05-06 12:39:57 +0200

    Fixed references to renderer.info

--- threex.rendererstats.js
+++ threex.rendererstats.js
@@ -7,10 +7,10 @@ var THREEx	= THREEx || {}
 
 /**
  * provide info on THREE.WebGLRenderer
- * 
+ *
  * @param {Object} renderer the renderer to update
  * @param {Object} Camera the camera to update
-*/
+ */
 THREEx.RendererStats	= function (){
 
 	var msMin	= 100;
@@ -27,13 +27,13 @@ THREEx.RendererStats	= function (){
 	msText.style.cssText = 'color:#f00;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px';
 	msText.innerHTML= 'WebGLRenderer';
 	msDiv.appendChild( msText );
-	
+
 	var msTexts	= [];
-	var nLines	= 9;
+	var nLines	= 10;
 	for(var i = 0; i < nLines; i++){
 		msTexts[i]	= document.createElement( 'div' );
 		msTexts[i].style.cssText = 'color:#f00;background-color:#311;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px';
-		msDiv.appendChild( msTexts[i] );		
+		msDiv.appendChild( msTexts[i] );
 		msTexts[i].innerHTML= '-';
 	}
 
@@ -52,15 +52,16 @@ THREEx.RendererStats	= function (){
 
 			var i	= 0;
 			msTexts[i++].textContent = "== Memory =====";
-			msTexts[i++].textContent = "Programs: "	+ webGLRenderer.info.memory.programs;
+			msTexts[i++].textContent = "Programs: "	+ webGLRenderer.info.programs.length;
 			msTexts[i++].textContent = "Geometries: "+webGLRenderer.info.memory.geometries;
 			msTexts[i++].textContent = "Textures: "	+ webGLRenderer.info.memory.textures;
 
 			msTexts[i++].textContent = "== Render =====";
+			msTexts[i++].textContent = "Frame: "	+ webGLRenderer.info.render.frame;
 			msTexts[i++].textContent = "Calls: "	+ webGLRenderer.info.render.calls;
-			msTexts[i++].textContent = "Vertices: "	+ webGLRenderer.info.render.vertices;
-			msTexts[i++].textContent = "Faces: "	+ webGLRenderer.info.render.faces;
+			msTexts[i++].textContent = "Triangles: "+ webGLRenderer.info.render.triangles;
+			msTexts[i++].textContent = "Lines: "	+ webGLRenderer.info.render.lines;
 			msTexts[i++].textContent = "Points: "	+ webGLRenderer.info.render.points;
 		}
-	}	
+	}
 };
